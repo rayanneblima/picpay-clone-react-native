427e27e3ca03759ee0e365065aff1a9b65d2dabf
Rayanne <rayanne_lima2010@hotmail.com>
2021-01-23 19:08:31 -0300

    Part 1 - Bottom Tabs Navigation

--- App.js
+++ App.js
@@ -1,21 +1,10 @@
-import { StatusBar } from 'expo-status-bar';
+import 'react-native-gesture-handler';
 import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
 
-export default function App() {
-  return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
-  );
+import App from './src/';
+
+function Main() {
+  return <App />;
 }
 
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
+export default Main;
---
+++ src/screens/Pay/index.js
@@ -0,0 +1,8 @@
+import React from 'react';
+import { Text } from 'react-native';
+
+function Pay() {
+  return <Text>Pay</Text>;
+}
+
+export default Pay;
